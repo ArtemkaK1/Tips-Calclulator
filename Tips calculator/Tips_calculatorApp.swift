46f351ef9378a73124436c42a0653ae20ce4b851
Artemiy Kirillov <akirillov@rtc.usetech.ru>
2022-05-10 11:27:18 +0300

    Initial Commit

---
+++ Tips calculator/Tips_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tips_calculatorApp.swift
+//  Tips calculator
+//
+//  Created by Artemiy Kirillov on 10.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Tips_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
